2750591efd1b79a1a20e56595e5d3ba71b663c6d
bhuatik609 <bhautikprajapati609@gmail.com>
2021-02-22 15:49:46 +0530

    done

--- model/deliveryModel.js
+++ model/deliveryModel.js
@@ -1,11 +1,11 @@
 var db=require('../dbconnection');
 var delivery={
     getAlldelivery:function(callback){
-        return db.query(' select *  from  delivery_tbl',callback);
+        return db.query('select d.*,e.*,o.* from delivery_tbl d,emp_tbl e,order_tbl o where e.emp_id=d.emp_id_fk,o.order_id=d.order_id_fk',callback);
     },
     adddelivery:function(data,callback){
 
-        return db.query('insert into delivery_tbl values(?,?,?,?,?)',[null,data.order_id_fk,data.emp_id_fk,data.del_date,data.del_status],callback);
+        return db.query('insert into delivery_tbl values(?,?,?,?,?)',[null,data.order_id_fk,data.del_date,data.del_status,data.emp_id_fk],callback);
     },
     deldelivery:function(id,callback){
         return db.query('delete from delivery_tbl where del_id=?',[id],callback);
